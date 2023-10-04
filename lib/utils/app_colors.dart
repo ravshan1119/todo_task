f80bfad4681b6db2e4e85b4aab455b8e0c983eb4
ravshan1119 <allamurodovravshan38@gmail.com>
2023-10-04 14:03:51 +0500

    colors, fonts, svg added.

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+
+class AppColors{
+  static Color black = const Color(0xFF000000);
+  static Color white = const Color(0xFFffffff);
+  static Color c_056ea2 = const Color(0xFF056ea2);
+  static Color c_009FEE = const Color(0xFF009FEE);
+  static Color c_C6E6F6 = const Color(0xFFC6E6F6);
+  static Color c_EE2B00 = const Color(0xFFEE2B00);
+  static Color c_c02200 = const Color(0xFFc02200);
+  static Color c_F6CFC6 = const Color(0xFFF6CFC6);
+  static Color c_EE8F00 = const Color(0xFFEE8F00);
+  static Color c_b86e00 = const Color(0xFFb86e00);
+  static Color c_F6E3C6 = const Color(0xFFF6E3C6);
+  static Color c_FEE8E9 = const Color(0xFFFEE8E9);
+  static Color grey100 = const Color(0xFFF3F4F6);
+}
